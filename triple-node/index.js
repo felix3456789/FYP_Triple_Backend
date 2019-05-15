5e64d62d681add50800d8dad0238589870ada94e
Chuen <chuen@Chuende-MacBook-Pro.local>
2019-05-15 14:56:08 +0800

    mongo

--- triple-node/index.js
+++ triple-node/index.js
@@ -1,13 +1,23 @@
 const debug = require('debug')('app:startup')
-const dbDebugger = require('debug')('app:db')
+const dbDebug = require('debug')('app:db')
 const config = require('config')
 const express = require("express")
 const Joi = require('joi')
 const morgan = require('morgan')
-const logger = require('./logger')
+const logger = require('./middleware/logger')
+const mongoose = require('mongoose')
 const app = express()
 
+const home = require('./routes/home')
+const courses = require('./routes/courses')
 
+
+mongoose.connect('mongodb://chuenpidb.tk:27017/triple', { useNewUrlParser: true })
+    .then(() => dbDebug('Connected to MongoDB'))
+    .catch(err => dbDebug('Could not connect to MongoDB ', err))
+
+app.set('view engine', 'pug')
+app.set('views', './views')
 
 debug(`NODE_ENV: ${process.env.NODE_ENV}`)
 debug(`app: ${app.get('env')}`)
@@ -16,6 +26,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+app.use('/', home)
+app.use('/app/courses', courses)
+
 debug('Application Name: ' + config.get('name'))
 
 if (app.get('env') === 'development') {
@@ -23,77 +36,7 @@ if (app.get('env') === 'development') {
     debug('Morgan enabled')
 }
 
-
 app.use(logger)
-
-app.use(function (req, res, next) {
-    console.log('Authenticating')
-    next()
-})
-
-const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' },
-]
-
-app.get('/', (req, res) => {
-    res.send('Hello!')
-})
-
-app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send('The course not found')
-    res.send(course)
-})
-
-app.post('/api/courses', (req, res) => {
-    const { error } = validateCourse(req.body)
-
-    if (error) return res.status(400).send(error.details[0].message)
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course)
-    res.send(course)
-})
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send('The course not found')
-
-    const { error } = validateCourse(req.body)
-
-    if (error) return res.status(400).send(error.details[0].message)
-
-    course.name = req.body.name
-    res.send(course)
-
-})
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send('The course not found')
-
-    const index = courses.indexOf(course)
-    courses.splice(index, 1)
-
-    res.send(course)
-})
-
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-    return Joi.validate(course, schema)
-}
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
