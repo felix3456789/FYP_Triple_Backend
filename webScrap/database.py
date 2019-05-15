46e0b7f3c118fa78860c4ea655a65519effa55dd
Chuen <chuen@Chuende-MacBook-Pro.local>
2019-05-15 19:02:31 +0800

    .

--- webScrap/database.py
+++ webScrap/database.py
@@ -4,8 +4,17 @@ myclient = pymongo.MongoClient("mongodb://chuenpidb.tk:27017/")
 mydb = myclient["triple"]
 mycol = mydb["tour"]
 
-def insertTour(dict):
-    x = mycol.insert_one(dict)
-    print('Inserted')
 
+
+def insertTour(dict):
+    temp = mycol.find_one({"_id":dict["_id"]})
+    if(not temp):
+        x = mycol.insert_one(dict)
+        print('Inserted!')
+    else:
+        myquery = { "_id": dict["_id"] }
+        newvalues = { "$set": dict }
+        mycol.update_one(myquery, newvalues)
+        print("Updated!")
+   
 
