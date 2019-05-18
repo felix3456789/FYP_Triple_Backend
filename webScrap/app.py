851bc6b9a37f9d8690fe91b8014bbf04bec202ee
Wilson Chu <wilsonchu11@gmail.com>
2019-05-19 00:28:05 +0800

    jump link + tags

--- webScrap/app.py
+++ webScrap/app.py
@@ -4,89 +4,119 @@ import codecs
 import database
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from lxml import etree
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-url = "https://tours.wingontravel.com/detail/japantravel-9-12461"
+url = "https://tours.wingontravel.com/"
 
 browser = webdriver.Chrome('./chromedriver')
 browser.get(url)
+searchLink = browser.find_element_by_css_selector("a[href*='japantravel']")
+searchLink.click()
+browser.switch_to_window(browser.window_handles[-1])
+tourLink = browser.find_elements_by_css_selector("a[href*='japantravel']")
+count = 0
+closePopUp = browser.find_element_by_css_selector("a[href*='javascript:MasterPageJS.appClose();']").click()
+url = browser.current_url
 
-response = requests.get(url)
-soup = BeautifulSoup(browser.page_source, "lxml")
+while (count < (len(tourLink) - 1)):
+    browser.get(url)
+    tourscrape = browser.find_elements_by_css_selector("a[href*='japantravel']")
+    if (count == (len(tourLink) - 2)):
+        browser.find_element_by_css_selector("a[class='next_page']").click()
+        browser.switch_to_window(browser.window_handles[-1])
+        tourLink = browser.find_elements_by_css_selector("a[href*='japantravel']")
+        url = browser.current_url
+        count = 0
+    tourscrape = browser.find_elements_by_css_selector("a[href*='japantravel']")
+    tourscrape[count].click()
+    browser.switch_to_window(browser.window_handles[-1])
 
-# find days
-days = soup.select('.segment_days')[0].findAll('li', recursive=False)
-tag = soup.select_one('.product_description').getText().lstrip()
-number_of_days = len(days)
+    response = requests.get(browser.current_url)
+    soup = BeautifulSoup(browser.page_source, "lxml")
 
-tags = tag.split("、")
-if(len(tags)>1):
-    allTag = tags
-if (len(tags) == 0):
-    allTag = " "
-if (len(tags) == 1):
-    allTag = soup.select_one('.product_description').getText().lstrip()
+    # find days
+    days = soup.select('.segment_days')[0].findAll('li', recursive=False)
+    tag = soup.select_one('.product_description').getText().lstrip()
+    number_of_days = len(days)
 
-availableDate = []
-dateList = soup.findAll("span",{"class":"on"})
-for i in range(len(dateList)):
-    date = dateList[i].attrs['date']
-    availableDate.append(date)
+    tags = tag.split("、")
+    if(len(tags)>1):
+        allTag = tags
+    if (len(tags) == 0):
+        allTag = " "
+    if (len(tags) == 1):
+        allTag = soup.select_one('.product_description').getText().lstrip()
+
+    availableDate = []
+    dateList = soup.findAll("span",{"class":"on"})
+    for i in range(len(dateList)):
+        date = dateList[i].attrs['date']
+        availableDate.append(date)
 
 
-image = []
-imageList = soup.select('.original_image')[0].findAll('img', {"src":True})
-for i in range(len(imageList)):
-    imagesrc = imageList[i].attrs['src']
-    image.append(imagesrc)
+    image = []
+    imageList = soup.select('.original_image')[0].findAll('img', {"src":True})
+    for i in range(len(imageList)):
+        imagesrc = imageList[i].attrs['src']
+        image.append(imagesrc)
 
-detail = soup.select('.brochure_tag')[0].findAll('a', {"href":True})
-detailLink = detail[0].attrs['href']
+    detail = soup.select('.brochure_tag')[0].findAll('a', {"href":True})
+    detailLink = detail[0].attrs['href']
 
-Tour = {
-    "_id": soup.select_one('.refCode').getText().lstrip().split('(')[1].split(')')[0],
-    "title": soup.select_one('.china_title').select_one('h2').getText().lstrip(),
-    "day": number_of_days,
-    "tag": allTag,
-    "price": soup.select_one('.price_box').select_one('div').select_one('em').getText().lstrip() + soup.select_one('.price_box').select_one('div').select_one('span').getText().lstrip(),
-    "availableDate": availableDate,
-    "image": image,
-    "detail": detailLink,
-    "Disable": False,
-    "Feature": False
-}
-
-for i in range(number_of_days):
-    class_name = '.route_day' + str(i)
-    day_info = soup.select(class_name)
-    if(day_info[0].select_one('.segment_content').select_one('p')):
-        content = day_info[0].select_one('.segment_content').select_one('p').getText().lstrip()
-    else:
-        content = " "
-
-    if(day_info[0].select_one('.stay')):
-        stay = day_info[0].select_one('.stay').getText().lstrip()
-    else:
-        stay = " "
-    
-    title = day_info[0].select_one('.segment_title').getText().lstrip().split("\n")
-
-    eat = []
-    eatList = day_info[0].findAll("li", {"class":"eat"})
-    for j in range(len(eatList)):
-        eat.append(eatList[j].getText().lstrip().split("；")[0].split("\n")[0])
-
-    Tour["day_" + str(i + 1)] = {
-        "day": day_info[0].select_one('.segment_day').getText().lstrip(),
-        "title": title[0],
-        "content": content,
-        "eat": eat,
-        "stay": stay
+    Tour = {
+        "_id": soup.select_one('.refCode').getText().lstrip().split('(')[1].split(')')[0],
+        "title": soup.select_one('.china_title').select_one('h2').getText().lstrip(),
+        "day": number_of_days,
+        "tags": allTag,
+        "price": soup.select_one('.price_box').select_one('div').select_one('em').getText().lstrip() + soup.select_one('.price_box').select_one('div').select_one('span').getText().lstrip(),
+        "availableDate": availableDate,
+        "image": image,
+        "detail": detailLink,
+        "Disable": False,
+        "Feature": False
     }
 
-tour_json = json.dumps(Tour,indent=2, ensure_ascii=False)
-print(tour_json)
+    for i in range(number_of_days):
+        class_name = '.route_day' + str(i)
+        day_info = soup.select(class_name)
+        if(day_info[0].select_one('.segment_content').select_one('p')):
+            content = day_info[0].select_one('.segment_content').select_one('p').getText().lstrip()
+        else:
+            content = " "
 
-#database.insertTour(Tour)
+        if(day_info[0].select_one('.stay')):
+            stay = day_info[0].select_one('.stay').getText().lstrip()
+        else:
+            stay = " "
+        
+        title = day_info[0].select_one('.segment_title').getText().lstrip().split("\n")
+
+        eat = []
+        eatList = day_info[0].findAll("li", {"class":"eat"})
+        for j in range(len(eatList)):
+            eat.append(eatList[j].getText().lstrip().split("；")[0].split("\n")[0])
+
+        Tour["day_" + str(i + 1)] = {
+            "day": day_info[0].select_one('.segment_day').getText().lstrip(),
+            "title": title[0],
+            "content": content,
+            "eat": eat,
+            "stay": stay
+        }
+
+    tour_json = json.dumps(Tour,indent=2, ensure_ascii=False)
+    print(tour_json)
+
+    database.insertTour(Tour)
+    count += 1
+
+    print(count)
+    print(len(tourLink))
+
 # print(Tour)
 
 # print()
