18829add08af3d81f0a0b17056a25a3033a9feb7
Wilson Chu <wilsonchu11@gmail.com>
2019-06-06 17:47:31 +0800

    .

--- webScrap/database.py
+++ webScrap/database.py
@@ -10,45 +10,33 @@ def insertTour(dict):
     temp = toursCol.find_one({"tourID":dict["tourID"]})
     if(not temp):
         _id = toursCol.insert_one(dict)
-        print('Inserted!')
-        print(_id.inserted_id)
         return _id.inserted_id
     else:
         myquery = { "tourID": dict["tourID"] }
         newValues = { "$set": dict }
         _id = toursCol.update_one(myquery, newValues)
-        print("Updated!")
-        print(temp["_id"])
         return temp["_id"]
 
 def insertTag(dict):
     tempTag = tagsCol.find_one({"title":dict["title"]})
     if(not tempTag):
         _id = tagsCol.insert_one(dict)
-        print('Inserted!')
-        print(_id.inserted_id)
         return _id.inserted_id
     else:
         myquery = { "title": dict["title"] }
         newValues = { "$set": dict }
         _id = tagsCol.update_one(myquery, newValues)
-        print("Updated!")
-        print(tempTag["_id"])
         return tempTag["_id"]
 
 def insertHashTag(dict):
     temp = hashtagsCol.find_one({"title":dict["title"]})
     if(not temp):
         _id = hashtagsCol.insert_one(dict)
-        print('Inserted!')
-        print(_id.inserted_id)
         return _id.inserted_id
     else:
         myquery = { "title": dict["title"] }
         newValues = { "$set": dict }
         _id = hashtagsCol.update_one(myquery, newValues)
-        print("Updated!")
-        print(temp["_id"])
         return temp["_id"]
 
 
@@ -59,7 +47,5 @@ def update_tags(new_tag, tourID):
             {'tourID': tourID}, 
             {"$addToSet": {"hashtags": new_tag}}
         )
-        print("Inserted!")
-        print(temp["_id"])
         return temp["_id"]
         
